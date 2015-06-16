49b21635315c75e92964ef562e3bfdb47f9b6a34
Murillo Nicacio de Maraes <mnmaraes@gmail.com>
2015-06-16 13:31:51 -0700

    Initial Commit v2

---
+++ Base/Base.h
@@ -0,0 +1,19 @@
+//
+//  Base.h
+//  Base
+//
+//  Created by Murillo Nicacio de Maraes on 6/16/15.
+//  Copyright (c) 2015 SuperUnreasonable. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Base.
+FOUNDATION_EXPORT double BaseVersionNumber;
+
+//! Project version string for Base.
+FOUNDATION_EXPORT const unsigned char BaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Base/PublicHeader.h>
+
+
